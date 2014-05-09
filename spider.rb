a6d0537ce166dce360f085701879c5daeef56778
Ankur Goel <ankurgel@gmail.com>
2014-05-09 15:14:35 +0530

    Completed crawling and indexing in Spider

--- spider.rb
+++ spider.rb
@@ -6,7 +6,6 @@ require 'nokogiri'
 require 'yaml'
 require 'webrobots'
 require 'fast-stemmer'
-#require 'debugger'
 require 'wirb'
 require 'awesome_print'
 require 'logger'
@@ -46,25 +45,100 @@ def scrub(link)
     return nil if RESTRICTED_TYPES.include? File.extname(link)
     URI.join(URI.escape(link)).normalize.to_s
   end
-  puts "Found #{site} <-- Crawling ..."
-  begin
-  doc = Nokogiri::HTML(open(site))
-  links = doc.css('a').map do |link|
-    if link['href']
-      link = link['href']
-      link = URI.join(site, link).to_s if link.start_with?("/")
-      link.to_s
-    else nil
+end
+
+COMMON_WORDS = File.read('common_words').split("\n")
+
+def add_to_index(url, doc)
+  # Indexing
+  link = Link.first_or_new(url: url)
+  if link.indexable?
+    puts "  Indexing #{url} at #{Time.now} <--"
+    #doc = Nokogiri::HTML(open(url))
+    link.locations.destroy unless link.new?
+    body = doc.at('body')
+    title = doc.at('title')
+    body.search('script, noscript').remove
+    words = body.text.gsub(/[\d]|[^\w\s]/, '').split.map(&:downcase).reject do |word|
+      COMMON_WORDS.include?(word) or word.size > 25
+    end.map(&:stem).each_with_index do |word, index|
+      loc = Location.new(position: index)
+      loc.word = Word.first_or_new(stem: word)
+      loc.link = link
+      puts "    --> Writing word - #{word} at index #{index}"
+      loc.save
     end
-  end.uniq.compact
-  p links
-  rescue OpenURI::HTTPError => e
-    puts "Couldn't connect - #{e.message}"
-  rescue Timeout::Error => e
-    puts "Request timed out! - #{e.message}"
-  rescue Exception => e
-    puts "Something went wrong - #{e.inspect} - #{e.message}"
+    puts "    --> Writing title - #{title.text}"
+    link.title = title.text
+    link.save
+    true
+  else
+    false
   end
 end
 
 
+#Traverse the seeds and start crawling ...
+loop do
+  accumulated_links = []
+  YAML.load(File.open(SEED)).map do |site|
+    begin
+      site = scrub(sanitize(site))
+    rescue
+      next
+    end
+    puts "Found the #{site} <-- Initializing procedure ..."
+    link = Link.first_or_new(url: site)
+    if link.new? or link.indexable?
+      begin
+        #if link.new? or link.indexable?
+        doc = Nokogiri::HTML(open(site))
+        add_to_index(site, doc)
+        puts "  Now crawling the #{site} for further links... <--"
+        links = doc.css('a').map do |link|
+          if link['href']
+            begin
+              link = link['href']
+              link = URI.join(site, link).to_s if link.start_with? "/"
+              link = scrub(link)
+              #link = scrub(URI.join(site, link).to_s) if link.start_with?("/")
+              link ? (robots.allowed?(URI(link)) ? link : nil) : link
+              accumulated_links << link
+
+              puts " -> #{link}"
+              #rescue OpenURI::HTTPError => e
+              #  puts "Couldn't connect - #{e.message}"
+              #  nil
+              #rescue Timeout::Error => e
+              #  puts "Request timed out! - #{e.message}"
+              #  nil
+            rescue Exception => e
+              puts "Something went wrong in here - #{e.inspect} - #{e.message}"
+              nil
+            end
+
+          else nil
+          end
+        end.uniq.compact
+
+        p links
+      rescue OpenURI::HTTPError => e
+        puts "Couldn't connect - #{e.message}"
+      rescue Timeout::Error => e
+        puts "Request timed out! - #{e.message}"
+      rescue Exception => e
+        puts "Something went wrong - #{e.inspect} - #{e.message}"
+      end
+      links ||= []
+    else
+      []
+    end
+  end
+  accumulated_links = accumulated_links.uniq.compact
+
+  File.open(SEED, 'wb') do |file|
+    file.puts("---")
+    accumulated_links.each { |link| file.puts "- #{link}" }
+  end
+
+end
